1f29b1ae3d57f37b7161ba12f6b44c5680b27921
yyj14 <yyj1453@naver.com>
2023-05-09 15:36:27 +0900

    05/09 test

---
+++ ch12_material/src/main/java/org/techtown/ch12_material/TwoFragment.kt
@@ -0,0 +1,23 @@
+package org.techtown.ch12_material
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.techtown.ch12_material.R
+
+
+class TwoFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_two, container, false)
+    }
+
+
+}
---
+++ tablayout/src/main/java/org/techtown/tablayout/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.techtown.tablayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.tabs.TabLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
